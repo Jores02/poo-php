a149c89c05212424009489c8210e54f6dd323ce2
AHOUANDOGBO Amen <112037382+Jores02@users.noreply.github.com>
2024-12-05 11:42:08 +0100

    Spécialisez vos objets avec les espaces de noms

--- index.php
+++ index.php
@@ -9,86 +9,39 @@
  * file that was distributed with this source code.
  */
 
+declare(strict_types=1);
 
+spl_autoload_register(static function ($fqcn): void {
+    $path = sprintf('%s.php', str_replace(['App\\Domain', '\\'], ['src', '/'], $fqcn));
+    require_once $path;
+});
 
- declare(strict_types=1);
+use App\Domain\MatchMaker\Encounter\Score;
+use App\Domain\MatchMaker\Lobby;
+use App\Domain\MatchMaker\Player\Player;
 
- class Lobby
- {
-     /** @var array<QueuingPlayer> */
-     public array $queuingPlayers = [];
- 
-     public function addPlayer(Player $player, int $range = 10): void
-     {
-         $this->queuingPlayers[] = new QueuingPlayer($player->getName(), $player->getRatio(), $range);
-     }
- 
-     public function findOponents(QueuingPlayer $player): array
-     {
-         $minLevel = round($player->getRatio() / 100);
-         $maxLevel = $minLevel + $player->range;
- 
-         return array_filter($this->queuingPlayers, static function (QueuingPlayer $potentialOponent) use ($minLevel, $maxLevel, $player) {
-             $playerLevel = round($potentialOponent->getRatio() / 100);
- 
-             return $player !== $potentialOponent && ($minLevel <= $playerLevel) && ($playerLevel <= $maxLevel);
-         });
-     }
- }
- 
+$greg = new Player('greg');
+$chuckNorris = new Player('Chuck Norris', 3000);
 
- class Player
- {
-     public function __construct(protected string $name, protected float $ratio = 400.0)
-     {
-     }
- 
-     public function getName(): string
-     {
-         return $this->name;
-     }
- 
-     private function probabilityAgainst(self $player): float
-     {
-         return 1 / (1 + (10 ** (($player->getRatio() - $this->getRatio()) / 400)));
-     }
- 
-     public function updateRatioAgainst(self $player, int $result): void
-     {
-         $this->ratio += 32 * ($result - $this->probabilityAgainst($player));
-     }
- 
-     public function getRatio(): float
-     {
-         return $this->ratio;
-     }
- }
- 
+$lobby = new Lobby();
+$lobby->addPlayer($greg);
+$lobby->addPlayer($chuckNorris);
 
- class QueuingPlayer extends Player
- {
-     public function __construct(string $name, float $ratio = 400.0, public int $range = 10)
-     {
-         parent::__construct($name, $ratio);
-     }
- }
- 
- 
- 
- $greg = new Player('greg', 400);
- $jade = new Player('jade', 476);
- 
- $lobby = new Lobby();
- $lobby->addPlayer($greg);
- $lobby->addPlayer($jade);
- 
- $opponents = $lobby->findOponents($lobby->queuingPlayers[0]);
- 
- var_dump($opponents);
- 
- exit(0);
- 
+while (count($lobby->queuingPlayers)) {
+    $lobby->createEncounters();
+}
 
+$encounter = end($lobby->encounters);
 
- 
- 
+// ces scores sont fictifs !
+$encounter->setScores(
+    new Score(score: 42, player: $greg),
+    new Score(score: 1, player: $chuckNorris)
+);
+
+var_dump($encounter);
+
+$encounter->updateRatios();
+
+var_dump($greg);
+var_dump($chuckNorris);
